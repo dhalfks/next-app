89800a42ab1a41d2c88ae1b98f8d3495a9a227dd
dhalfks <miran5093@gmail.com>
2026-06-08 12:24:01 +0900

    next-app 3

--- app/components/inputSample.tsx
+++ app/components/inputSample.tsx
@@ -6,15 +6,27 @@ export default function InputSample(){
     // useState 관리
     const [text, setText] = useState<string>(''); // 문자타입 초기설정값 빈값
 
+    // e :  event 발생하는 대상의 객체 , target 객체 (element)
+    // React.ChangeEvent<대상 element>
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        // name=key / value=value
+        setText(e.target.value);
+    }
+
     return(
         <div className="m-5">
             <input className="border border-gray-300 p-2"
                 type="text" 
                 name="text" 
-                value={text} 
+                value={text}
+                placeholder="text..."
+                onChange={onChange} 
             />
             <button className="px-4 py-2 ml-3 bg-blue-500 text-white "
+                onClick={()=>setText('')}
             >reset</button>
+
+            <div className="mt-2">값 : {text}</div>
 
         </div>
     )
---
+++ app/components/inputSample2.tsx
@@ -0,0 +1,12 @@
+export default function InputSample2(){
+
+    
+    return(
+        <div>
+            <input type="text" name="id" placeholder="ID..." />
+            <input type="text" name="nick" placeholder="NickName..." />
+            <button> reset </button>
+            <div> 값: id / nick </div>
+        </div>
+    )
+}
